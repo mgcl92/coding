ae0586bcef179ef58c08aeda0b55fa94453ce398
mgkl <zst_lff@126.com>
2023-11-14 13:20:45 +0800

    img_processor.py 代码简化修改

---
+++ crypto_impl/caesar.py
@@ -0,0 +1,7 @@
+ALPHABET = ()
+def Caesar(pt):
+    sk = 3
+    global  ct
+    for element in pt:
+        ct = ALPHABET()
+    return
--- some _attempt/img_processor.py
+++ some _attempt/img_processor.py
@@ -5,19 +5,26 @@ def image_processor(img_path, block_size, block_processor):
     img = Image.open(img_path)
     width, height = img.size
 
-    cols = width // block_size
-    rows = height // block_size
-    for i in range(cols):
-        for j in range(rows):
-            left = i * block_size;
-            top = j * block_size
-            right = left + block_size;
-            bottom = top + block_size
-            block = img.crop((left, top, right, bottom))
+    # cols = width // block_size
+    # rows = height // block_size
+    #
+    # for i in range(cols):
+    #     for j in range(rows):
+    for i in range(width // block_size):
+        for j in range(height // block_size):
+            # left = i * block_size;
+            # top = j * block_size
+            # right = left + block_size;
+            # bottom = top + block_size
+            coordinates = (i * block_size, j * block_size, (i + 1) * block_size, (j + 1) * block_size)
+
+            # block = img.crop((left, top, right, bottom))
+            block = img.crop(coordinates)
 
             processed_block = block_processor(block)
 
-            img.paste(processed_block, (left, top, right, bottom))
+            # img.paste(processed_block, (left, top, right, bottom))
+            img.paste(processed_block, coordinates)
 
     img.show()
 
@@ -26,17 +33,23 @@ def block_processor(block):
     width, height = block.size
 
     sum = [0, 0, 0]
-    for i in range(width):
-        for j in range(height):
-            pixel = block.getpixel((i, j))
-            for k, _ in enumerate(sum):
-                sum[k] += pixel[k]
+    # for i in range(width):
+    #     for j in range(height):
+    #         pixel = block.getpixel((i, j))
+    #         for k, _ in enumerate(sum):
+    #             sum[k] += pixel[k]
+
+    for pixel in list(block.getdata()):
+        for i, _ in enumerate(pixel):
+            sum[i] += pixel[i]
 
     for i, _ in enumerate(sum):
         sum[i] //= width * height
 
-    datas = [tuple(sum)] * width * height
-    block.putdata(datas)
+    # datas = [tuple(sum)] * width * height
+
+    # block.putdata(datas)
+    block.putdate([tuple(sum)] * width * height)
 
     return block
 
